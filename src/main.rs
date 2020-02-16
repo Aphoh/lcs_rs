e5d9be37678c6f729181ebb7a75672604c1baf80
William Arnold <will748@gmail.com>
2020-02-15 17:14:52 -0800

    Correct impl, needs distinct sentinels

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,18 @@ use std::collections::HashSet;
 const K: u32 = 2;
 
 fn main() {
-    let data = vec!["ABC", "BCD", "ABB"];
+    let data = vec!["093AB", "0AB435AB", "0K093KABB"];
     let n_strings = data.len(); 
-    let lengths : Vec<usize> = data.iter().map(|s| s.len()).collect();
-    
-    let mut combined = String::with_capacity(lengths.iter().sum::<usize>() + data.len());
-    let mut non_essential_pos : HashSet<usize> = HashSet::new();
+    let total_length : usize = data.iter().map(|s| s.len()).sum(); 
+    let mut combined = String::with_capacity(total_length);
+    let mut sentinel_pos_set : HashSet<usize> = HashSet::new();
+    let mut sentinel_pos : Vec<usize> = Vec::new();
     for s in data {
         combined.push_str(&s);
         combined.push('$');
-        non_essential_pos.insert(combined.len() - 1);
+        let sent_ind = combined.len() - 1; 
+        sentinel_pos.push(sent_ind);
+        sentinel_pos_set.insert(sent_ind);
     }
 
     print!("{}\n", combined);
@@ -25,11 +27,13 @@ fn main() {
     let suffix_array = suffix_table.table();
     let lcp_array: Vec<u32> = suffix_table.lcp_lens();
    
-    let l0 = get_l0(&suffix_table, &non_essential_pos, &lengths);
+    let l0 = get_l0(&suffix_table, &sentinel_pos_set, &sentinel_pos);
 
     print!("{:?}\n", suffix_table);
     print!("L0: {}\n", &l0);
-    print!("LCP Array:\n{:0>2?}\n", lcp_array);
+    for (i, l) in lcp_array.iter().enumerate() {
+        println!("lcp_array[{}]: {}", i, l);
+    }
     
     let start_ind = n_strings;
     let end_ind = l0 - 1;
@@ -39,33 +43,31 @@ fn main() {
 
     delta_ls[0] = start_ind;
     delta_rs[0] = start_ind + 1;
-    if !non_essential_pos.contains(&start_ind) {
-        let s_ind = get_string_index(suffix_array[start_ind] as usize, &lengths);
+    if let Some(s_ind) = get_string_index(suffix_array[start_ind] as usize, &sentinel_pos) {
         type_counters[s_ind] += 1;
     }
+
     //Advance until K-good
     while count_nonzero(&type_counters) < K {
         delta_rs[0] += 1;
         let i = delta_rs[0] - 1; //The new character that is in delta_0
-        let s_ind = get_string_index(suffix_array[i] as usize, &lengths);
-        type_counters[s_ind] += 1;
+        if let Some(s_ind) = get_string_index(suffix_array[i] as usize, &sentinel_pos) {
+            type_counters[s_ind] += 1;
+        }
     }
 
-    //delta_n+1 is now k-good, can advance
-
+    //delta_n+1 is now K-good, can advance
     for (j, i) in (start_ind..(end_ind + 1)).enumerate().skip(1){
         delta_ls[j] = i;
         delta_rs[j] = delta_rs[j - 1];
         //Remove one from the type counter corresponding to the i-1-th char
-        if !non_essential_pos.contains(&(i-1)) { 
-            let ls_ind = get_string_index(suffix_array[i-1] as usize, &lengths);
+        if let Some(ls_ind) = get_string_index(suffix_array[i-1] as usize, &sentinel_pos) {
             type_counters[ls_ind] -= 1;
         }
         while count_nonzero(&type_counters) < K {
             let new_char_ind = delta_rs[j];
             delta_rs[j] += 1;
-            if !non_essential_pos.contains(&new_char_ind) { 
-                let s_ind = get_string_index(suffix_array[new_char_ind] as usize, &lengths);
+            if let Some(s_ind) = get_string_index(suffix_array[new_char_ind] as usize, &sentinel_pos) {
                 type_counters[s_ind] += 1;
             }
         }
@@ -75,26 +77,26 @@ fn main() {
    
     let mut maxi = 0usize;
     let mut maxv = 0u32;
-    for i in 0..delta_ls.len(){
-        let mut min_lcp = u32::MAX;
-        for j in delta_ls[i]..delta_rs[i] {
-            if min_lcp > lcp_array[j - 1]{
-                min_lcp = lcp_array[j - 1];
-            }
-        }
-        if min_lcp >= maxv {
-            maxv = min_lcp;
+    let result = delta_ls.iter().zip(&delta_rs).enumerate()
+        .map(|(i, (&l, &r))| (i, lcp_array[(l + 1)..r].iter().min().unwrap_or(&0)));
+
+    println!("(delta, min_lcp) pairs:");
+    for (i, lcp_min) in result {
+        println!("({}:{}, {})", &delta_ls[i], &delta_rs[i], &lcp_min);
+        if lcp_min > &maxv {
+            maxv = *lcp_min;
             maxi = i;
         }
     }
 
-    print!("Got max delta {} with min_lcp {}\n", maxi, maxv);
+    println!("Got max delta {} with min_lcp {}", maxi, maxv);
 
     let lcs_start = suffix_array[delta_ls[maxi]] as usize;
-    let lcs_end = lcs_start + (maxv as usize) + 1;
+    let lcs_end = lcs_start + (maxv as usize);
 
     let lcs = &combined[lcs_start..lcs_end];
-    print!("Result: {:?}\n", lcs);
+    println!("Result: {:?}", lcs);
+        
 }
 
 fn count_nonzero(vec: &Vec<u32>) -> u32 {
@@ -108,28 +110,24 @@ fn count_nonzero(vec: &Vec<u32>) -> u32 {
 }
 
 fn get_l0(suffix_table: &SuffixTable, 
-          invalid_pos: &HashSet<usize>, 
-          string_lengths: &Vec<usize>) -> usize {
+          sentinel_pos_set: &HashSet<usize>, 
+          sentinel_pos: &Vec<usize>) -> usize {
     
-    let mut present_strs = HashSet::<u32>::new(); 
+    let mut present_strs = HashSet::<usize>::new(); 
     let total_len = suffix_table.text().len();
     let suffix_array = suffix_table.table();
 
     //Find L_0
-    let mut n_strings = vec![K + 1; total_len];
+    let mut n_strings: Vec<usize> = vec![(K as usize) + 1; total_len];
     for i in (0..total_len).rev(){
-        n_strings[i] = if !invalid_pos.contains(&i) {
-            let si = get_string_index(suffix_array[i] as usize, &string_lengths);
-            present_strs.insert(si as u32);
-            present_strs.len() as u32
-        } else {
-            K + 1 
+        if let Some(si) = get_string_index(suffix_array[i] as usize, &sentinel_pos) {
+            present_strs.insert(si);
+            n_strings[i] = present_strs.len()
         }
     }
-    print!("{:?}\n", n_strings);
     let mut l0 = total_len;
     for (i, k) in n_strings.iter().enumerate() {
-        if k < &K {
+        if k < &(K as usize) {
             l0 = i - 1; //As [i, L] has at least K distinct essenital types
             break;
         }
@@ -137,14 +135,13 @@ fn get_l0(suffix_table: &SuffixTable,
     l0 + 1
 }
 
-fn get_string_index(pos: usize, string_lengths: &Vec<usize>) -> usize {
-   let mut curr = 0; 
-   let mut acc_len = 0;
-   while pos > acc_len + string_lengths[curr] {
-       acc_len += string_lengths[curr] + 1;
-       curr+=1;
-   }
-   curr
+fn get_string_index(pos: usize, sentinel_pos: &Vec<usize>) -> Option<usize> {
+    let mut i = 0;
+    while pos > sentinel_pos[i] {
+        i+=1;
+    }
+
+    if sentinel_pos[i] == pos { None } else { Some(i) }
 }
 
 /*fn as_hex(bytes: Vec<u8>) -> String {
